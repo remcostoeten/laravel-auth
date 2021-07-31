0b22484d0a4fe571b73d5494f8a5b9e0a5be18fe
remcostoeten <remco@tickles.nl>
2021-07-31 22:49:30 +0200

    Initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create(
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => $request->input('password')
+        ]);
+    }
+
+
+    public function user()
+    {
+        return 'Authenticated user';
+    }
+}
